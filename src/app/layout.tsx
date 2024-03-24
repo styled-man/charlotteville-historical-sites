fc4f65ad1e13f7960f82afc76c1c48cc19ae758e
Ruth Velasquez <Rvela058@fiu.edu>
2024-03-24 09:01:42 -0400

    did homepage

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,10 @@
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import { Inter, Gowun_Batang } from 'next/font/google'
 import './globals.tw.css'
 import Providers from '@/providers'
 
 const inter = Inter({ subsets: ['latin'] })
+const gowun_batang = Gowun_Batang({ subsets: ['latin'],weight: ['700','400']})
 
 export const metadata: Metadata = {
   title: 'Charlotteville VA Historical Sites',
@@ -16,7 +17,7 @@ interface RootLayoutProps {
 export default function RootLayout({ children }: Readonly<RootLayoutProps>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={inter.className + " " + gowun_batang.className}>
         <Providers>{children}</Providers>
       </body>
     </html>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,78 @@
 'use client'
 
 import { Map, AdvancedMarker, Pin } from '@vis.gl/react-google-maps'
+import Image from 'next/image'
+import Link from 'next/link'
 
 export default function Home() {
   const cords = { lat: 38.0831, lng: -78.4344 }
 
   return (
+    <div className="grid grid-cols-9 bg-[#DDD0C8]">
+      <aside className="col-span-1 px-4 py-4 h-[20vh]">
+        <Image width={120} height={120} alt="Charlotteville" src="/images/image 2.png" />
+      </aside>
+      <main className="col-span-8">
+        <span>
+          <h1 className="text-7xl font-gorwun-batang px-3 py-3 flex-1 flex-grow-0">
+            <div className="flex flex-col">
+              <span>Find Historic Sites in</span>
+              <span className="flex items-center gap-3 ">
+                <span>Charlottesville</span>
+                <div className="flex flex-col gap-2.5 ">
+                  <div className="h-[2px] w-32 bg-black"></div>
+                  <div className="h-[2px] w-32 bg-black"></div>
+                  <div className="h-[2px] w-32 bg-black"></div>
+                  <div className="h-[2px] w-32 bg-black"></div>
+                  <div className="h-[2px] w-32 bg-black"></div>
+                </div>
+              </span>
+            </div>
+          </h1>
+        </span>
+
+        <div className="relative w-full h-[75.8vh]">
+          <Image fill alt="Charlottesville" src="/images/longPhoto.jpg" className="object-cover" />
+          <Link
+            href="/search"
+            className="h-20 w-40 px-4 m-2 flex text-3xl items-center justify-center absolute right-40 bottom-40 bg-white text-black font-semibold rounded border-r-5 bg-opacity-90"
+          >
+            Search
+          </Link>
+          {/* <button className="p-2 bg-black text-white right-0 bottom-0 border-r-transparent font-gorwun-batang hover:bg-white hover:text-black">
+            <a href="">Search</a>
+          </button> */}
+        </div>
+
+        <div>
+          {/*   <Image width={1500} height={1200} alt='Charlottesville' src='/Images/longPhoto.jpg' /> */}
+
+          {/* <img  alt='Charlottesville' src='/Images/longPhoto.jpg' className='h-full w-full '/>
+        <button className='p-2 bg-black text-white absolute border-r-transparent font-gorwun-batang hover:bg-white hover:text-black'>
+          <a href="">Search</a>
+        </button> */}
+        </div>
+      </main>
+    </div>
+  )
+
+  return (
     <main className="">
-      <div className="w-screen h-screen">
-        <Map defaultZoom={15} defaultCenter={cords} mapId={process.env.NEXT_PUBLIC_API_GOOGLE_MAP_ID}>
-          <AdvancedMarker position={cords}>
-            <Pin />
-          </AdvancedMarker>
-        </Map>
+      <div className="w-full">
+        <div>
+          <Image width={120} height={120} alt="Charlotteville" src="/Images/image 2.png" />
+        </div>
+        <div>
+          <header>
+            <h1 className="text-7xl font-gorwun-batang">
+              Find Historic Sites in <br></br> Charlottesville
+            </h1>
+          </header>
+        </div>
       </div>
+      <span className="mainImage w-full">
+        <Image width={975} height={1080} alt="Charlottesville" src="/Images/downtownMall.jpg" />
+      </span>
     </main>
   )
 }
