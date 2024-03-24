9fc9fa54bc582a15a9dab999ec720c2c43f731aa
Carol <apowe056@fiu.edu>
2024-03-24 09:01:08 -0400

    completed the search page

---
+++ src/app/search/page.tsx
@@ -0,0 +1,42 @@
+'use client'
+import Image from 'next/image'
+import { Map, AdvancedMarker, Pin } from '@vis.gl/react-google-maps'
+
+export default function Search() {
+  const cords = { lat: 38.0831, lng: -78.4344 }
+
+  return (
+    <div className="grid grid-cols-9 h-screen bg-[#DDD0C8]">
+      <aside className="col-span-1 px-4 py-4">
+        <Image width={100} height={100} className="" alt="Charlotteville" src="/images/image 4.png" />
+      </aside>
+
+      <div className="col-span-8">
+        <div className="flex items-center justify-center h-28 gap-4">
+          <div className="flex flex-col gap-2.5 ">
+            <div className="h-[2px] w-96 bg-black"></div>
+            <div className="h-[2px] w-96 bg-black"></div>
+            <div className="h-[2px] w-96 bg-black"></div>
+            <div className="h-[2px] w-96 bg-black"></div>
+            <div className="h-[2px] w-96 bg-black"></div>
+          </div>
+          <h1 className="text-5xl font-normal bg-black text-white p-4 rounded-lg">Search</h1>
+          <div className="flex flex-col gap-2.5 ">
+            <div className="h-[2px] w-96 bg-black"></div>
+            <div className="h-[2px] w-96 bg-black"></div>
+            <div className="h-[2px] w-96 bg-black"></div>
+            <div className="h-[2px] w-96 bg-black"></div>
+            <div className="h-[2px] w-96 bg-black"></div>
+          </div>
+        </div>
+    <div className='h-[500px] mr-48 ml-48 mt-18 mb-10 rounded-xl overflow-hidden'>
+        <Map defaultZoom={15} defaultCenter={cords} mapId={process.env.NEXT_PUBLIC_API_GOOGLE_MAP_ID}>
+          <AdvancedMarker position={cords}>
+            <Pin />
+          </AdvancedMarker>
+        </Map>
+        </div>
+      </div>
+    </div>
+  )
+}
